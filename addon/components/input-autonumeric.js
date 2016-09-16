4ae29a125e3a75f6170776c2c9d291b3bc786f73
Tobias Södergren <tobias.sodergren@infomaker.se>
2016-09-16 20:48:18 +0200

    Added config options

--- addon/components/input-autonumeric.js
+++ addon/components/input-autonumeric.js
@@ -2,6 +2,42 @@ import Ember from 'ember';
 
 export default Ember.TextField.extend({
 
+  aSep: ',',
+  dGroup: '3',
+  aDec: '.',
+  altDec: null,
+  aSign: '',
+  pSign: 'p',
+  vMin: '0.00',
+  vMax: '999999999.99',
+  mDec: null,
+  mRound: 'S',
+  aPad: true,
+  nBracket: null,
+  wEmpty: 'empty',
+  IZero: 'allow',
+  aForm: true,
+
+  options: function() {
+    return {
+      aSep: this.get('aSep'),
+      dGroup: this.get('dGroup'),
+      aDec : this.get('aDec'),
+      altDec : this.get('altDec'),
+      aSign : this.get('aSign'),
+      pSign : this.get('pSign'),
+      vMin : this.get('vMin'),
+      vMax : this.get('vMax'),
+      mDec : this.get('mDec'),
+      mRound : this.get('mRound'),
+      aPad : this.get('aPad'),
+      nBracket : this.get('nBracket'),
+      wEmpty : this.get('wEmpty'),
+      IZero : this.get('IZero'),
+      aForm : this.get('aForm')
+    };
+  },
+
   initializeAutoNumeric: function () {
     this.$().autoNumeric('init', {aSep: ' ', aDec: ',', mDec: '0'});
     this.$().autoNumeric('set', this.get('number'));
@@ -9,10 +45,9 @@ export default Ember.TextField.extend({
 
   tearDownAutoNumeric: function () {
     this.$().autoNumeric('destroy');
-  },
+  }.on('willDestroyElement'),
 
   getValue: function () {
-    console.log('get-value', this.$().autoNumeric('get'));
     this.set('number', parseFloat(this.$().autoNumeric('get')));
   }.observes('value')
 
